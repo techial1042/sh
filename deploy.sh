86817aba0cea5d776443e0514b428e505152d1af
techial <me@techial.top>
2020-05-22 20:55:25 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 
 
-sudo yum install wget curl zsh git htop nginx dnf -y
+sudo yum install wget curl zsh git htop nginx dnf nmap yum-utils -y
 
+# email
+systemctl --now disable postfix
+
+# nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# openjdk
 sudo yum install java-11-openjdk-devel -y
 
+# node
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-
 sudo yum install nodejs -y
 
+# yarn
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-
 sudo yum install yarn -y
+
+# docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
 
 sudo yum upgrade -y
