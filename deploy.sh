d9576ce1605bb1e8907bb4f38f9004dde0d62851
techial <me@techial.top>
2020-05-30 10:30:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,7 @@ sudo yum remove docker \
                   docker-logrotate \
                   docker-engine
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-ce docker-ce-cli containerd.io --nobest -y
 sudo systemctl --now enable docker
 
 # docker-compose 
